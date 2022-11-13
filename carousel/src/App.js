a96d4e40db99f15ea4d92e5ea59771bd3180d4b4
praakhya <67508200+praakhya@users.noreply.github.com>
2022-11-13 19:20:03 +0530

    added default items in carousel

--- carousel/src/App.js
+++ carousel/src/App.js
@@ -14,39 +14,45 @@ function App() {
     color: "black",
     backgroundColor: "green"
   }
+  var itemArr=[
+    <div className='item'>1</div>,
+    <div className='item'>2</div>,
+    <div className='item'>3</div>,
+    <div className='item'>4</div>,
+    <div className='item'>5</div>,
+    <div className='item'>6</div>,
+    <div className='item'>7</div>,
+    <div className='item'>8</div>,
+    <div className='item'>9</div>,
+    <div className='item'>10</div>,
+    <div className='item'>11</div>,
+    <div className='item'>12</div>,
+    <div className='item'>13</div>,
+    <div className='item'>14</div>,
+    <div className='item'>15</div>,
+    <div className='item'>16</div>,
+    <div className='item'>17</div>,
+    <div className='item'>18</div>,
+    <div className='item'>19</div>,
+    <div className='item'>20</div>,
+    <div className='item'>21</div>,
+    <div className='item'>22</div>,
+    <div className='item'>23</div>,
+    <div className='item'>24</div>
+  ]
   return (
     <div className="App">
       <br/>
       <br/>
       <br/>
       <ScrollProvider value={{first:0, count:3}}>
-        <Carousel containerClass="container" countVisible={3} shiftAmount={3}>
-          {[
-            <div className='item'>1</div>,
-            <div className='item'>2</div>,
-            <div className='item'>3</div>,
-            <div className='item'>4</div>,
-            <div className='item'>5</div>,
-            <div className='item'>6</div>,
-            <div className='item'>7</div>,
-            <div className='item'>8</div>,
-            <div className='item'>9</div>,
-            <div className='item'>10</div>,
-            <div className='item'>11</div>,
-            <div className='item'>12</div>,
-            <div className='item'>13</div>,
-            <div className='item'>14</div>,
-            <div className='item'>15</div>,
-            <div className='item'>16</div>,
-            <div className='item'>17</div>,
-            <div className='item'>18</div>,
-            <div className='item'>19</div>,
-            <div className='item'>20</div>,
-            <div className='item'>21</div>,
-            <div className='item'>22</div>,
-            <div className='item'>23</div>,
-            <div className='item'>24</div>
-          ]}
+        <Carousel 
+        containerClass="container" 
+        itemClass="item"
+        itemContainerClass="itemContainer"
+        arrowContainerClass="arrows"
+        countVisible={3} 
+        shiftAmount={3}>
         </Carousel>
       </ScrollProvider>
     </div>
